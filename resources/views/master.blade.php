cd8f6f3b517d57a2d77c71d3de6903592c17ef49
Drashti2299 <drashtibrahmbhatt2210@gmail.com>
2022-02-08 12:22:58 +0530

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+
+	<title>Ecomerce project</title>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	{{View::make('header')}}
+	@yield('content')
+	{{View::make('footer')}}
+
+</body>
+<style >
+	.custom-login{
+		height: 500px;
+		padding-top: 100px;
+	}
+</style>
+
+</html>
